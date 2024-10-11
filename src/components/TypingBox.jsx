bfe00004c6ac8a697f7f95af76d5aef2b17b14bc
skipperr254 <skipperr254@gmail.com>
2024-10-11 10:23:40 +0300

    Allow text to be written on top of the given text

--- src/components/TypingBox.jsx
+++ src/components/TypingBox.jsx
@@ -3,9 +3,15 @@ import './TypingBox.css'
 
 const TypingBox = () => {
     const [isFocused, setIsFocused] = useState(false);
+    const [currentKey, setCurrentKey] = useState('');
+    const [value, setValue] = useState('');
 
     const handleKeyDown = (event) => {
-        console.log(event.key)
+        setCurrentKey(event.key)
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
     }
 
     return (
@@ -18,12 +24,13 @@ const TypingBox = () => {
         >
             {!isFocused && <div className="overlay"><p>Click here to focus</p></div>}
             <div className={`typing-box ${isFocused ? "focused" : ""}`}>
-                <p>
+                <p className="typing-text">
                     possible life late the real great any time about and last these early a present such seem go
                     again much for during open be end program make program get who more about too play these present
                     when person with how so down over again up one some while keep end
                 </p>
             </div>
+            <textarea className="text-input" name="typed-text" id="typed-text" onChange={handleChange} value={value}></textarea>
         </div>
     )
 }
